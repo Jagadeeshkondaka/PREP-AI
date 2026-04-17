6c09c6e6ca46554dab75dd2a950235c34f63f1ec
pavanmajhi05-sys <jagadeeshkondaka123@gmail.com>
2026-04-17 16:20:10 +0530

    final commit

--- frontend/src/pages/Resource.jsx
+++ frontend/src/pages/Resource.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import Navbar from "../layout/Navbar";
-import API from "../config";
+
 
 const Resources = () => {
   const [query, setQuery] = useState("");
